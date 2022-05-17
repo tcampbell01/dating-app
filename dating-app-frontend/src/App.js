be899a628f3d9c04dccf78e8a498817909161fc1
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-05-16 21:44:00 -0400

    added header components

--- dating-app-frontend/src/App.js
+++ dating-app-frontend/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dating App MERN</h1>
-      </div>);
+    <div className="app">
+      <Header />
+     
+      </div>
+      );
 }
      
 
